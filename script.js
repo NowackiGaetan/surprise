6ed0ee056f9f37d25198658fc02d6945e19a8990
unknown <nowacki.gaetan@homtail.com>
2024-04-04 14:25:35 +0200

    ajustement style

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const button = document.getElementById("button-password");
 const password = document.getElementById("password");
 const box = document.getElementById("box-surprise");
+const boxError = document.getElementById("box-surprise-error");
 
 box.style.display = "none";
+boxError.style.display = "none";
 
 button.addEventListener("click", () => {
-    if (password.value === "password") {
+    if (password.value === "Starfoulah") {
         box.style.display = "block";
+        boxError.style.display = "none";
+    }else{
+        boxError.style.display = "block";
+        box.style.display = "none";
     }
 });
